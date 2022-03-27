77619ae6483266f618228c8eebf723718a8447ab
liuqs <liuqs29@mail.ustc.edu.cn>
2022-03-27 16:56:58 +0800

    build basic spring from scratch

--- small-spring-step-19/src/main/java/cn/bugstack/springframework/core/annotation/SynthesizedAnnotation.java
+++
@@ -1,7 +0,0 @@
-package cn.bugstack.springframework.core.annotation;
-
-/**
- * @author zhangdd on 2022/2/27
- */
-public @interface SynthesizedAnnotation {
-}
--- small-spring-step-19/src/main/java/cn/bugstack/springframework/jdbc/core/SqlProvider.java
+++
@@ -1,9 +0,0 @@
-package cn.bugstack.springframework.jdbc.core;
-
-/**
- * @author zhangdd on 2022/2/9
- */
-public interface SqlProvider {
-
-    String getSql();
-}
